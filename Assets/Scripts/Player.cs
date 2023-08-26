687be149fd6099831391eff4eeeb057aec004dc7
wizLucky <nagadon0424@icloud.com>
2023-08-26 17:14:39 +0900

    add Player.cs

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,53 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private float speed = 5f;
+    
     void Start()
     {
         
     }
-
-    // Update is called once per frame
+    
+    
     void Update()
     {
+        Vector3 moveVector = GetMoveVector();
+        bool isMove = moveVector != Vector3.zero;
+
+        if (isMove)
+        {
+            Move(moveVector);
+        }
         
+    }
+    
+    private Vector3 GetMoveVector()
+    {
+        Vector3 moveVector = Vector3.zero;
+        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
+        {
+            moveVector += Vector3.forward;
+        }
+        if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
+        {
+            moveVector += Vector3.back;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        {
+            moveVector += Vector3.left;
+        }
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        {
+            moveVector += Vector3.right;
+        }
+        return moveVector.normalized;
+    }
+
+    void Move(Vector3 moveVector)
+    {
+        // 移動処理
+        Vector3 moveDelta;
+        moveDelta = moveVector * Time.deltaTime * speed;
+        transform.position += moveDelta;
     }
 }
