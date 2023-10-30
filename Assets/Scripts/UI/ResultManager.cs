ec95a929430cbea74319a719c7c86482c9820dbd
Begirama3 <107602752+Begirama3@users.noreply.github.com>
2023-10-31 05:41:05 +0900

    add リザルトﾗﾝｷﾝｸﾞ.cs

---
+++ Assets/Scripts/UI/ResultManager.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+public class ResultManager : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI timetext;
+    [SerializeField] TextMeshProUGUI heighttext;
+
+    private void Start()
+    {
+        // GoalTime‚ðŽæ“¾
+        float nowtime = UIManager.LoadNowTime();
+        int nowheight = UIManager.LoadNowHeight();
+
+        // GoalTime‚ð•\Ž¦
+        timetext.text = "Time: " + nowtime.ToString() + " s";
+        heighttext.text = "Height: " + nowtime.ToString() + " %";
+
+    }
+}
